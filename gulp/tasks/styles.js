9cad6c5d49e6c08da8e3befdb17ce2ed0be1de23
Aleksei Arro <polsck@gmail.com>
2019-12-21 17:28:03 +0300

    Autoprefixer added

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,15 @@
 const { src, dest } = require('gulp');
-const stylus = require('gulp-stylus');
 const debug = require('gulp-debug');
+const stylus = require('gulp-stylus');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-module.exports = () => src(`./src/stylus/index.styl`)
+const postcssPlugins = [autoprefixer];
+
+module.exports = () => src(`./src/stylus/**/*.styl`)
   .pipe(debug({ title: 'styles' }))
   .pipe(stylus())
+  .pipe(debug({ title: 'postcss' }))
+  .pipe(postcss(postcssPlugins))
   .pipe(debug({ title: 'dest' }))
   .pipe(dest(`public/assets`));
